83d6652fe00ce52b3c6476e184143ac2cb5c5b63
Vijay <vijay@finflock.com>
2024-12-05 16:56:03 +0530

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, send_from_directory
 import os
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_port=1, x_for=1, x_host=1, x_prefix=1)
 
 # Directory to monitor
 DIRECTORY_PATH = "test"
